071f5d04ce13f7b81c85b4cfc291c77d6f418ebb
CristianJRamirez <45858000w@iespoblenou.org>
2016-11-07 20:25:03 +0100

    Crear vista de carta seleccionada, listado de cartas con imagenes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,12 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import org.json.JSONException;
 
 import java.io.BufferedInputStream;
@@ -14,6 +16,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -27,25 +30,87 @@ public class Controller {
     public RadioButton rdbtEspecial;
     public RadioButton rdbtMistic;
     public RadioButton rdbtBasicLand;
+    public TextArea txtAreaText;
+    public TextField txtRarity;
+    public TextField txtType;
+    public TextField txtPower;
+    public TextField txtMana;
+    public TextField txtName;
+    public ImageView Imagen;
 //endregion
 
+    public ArrayList<String> imagenes = new ArrayList<String>();
 
-    public void initialize(){
+    public ObservableList<String> listaCartas = FXCollections.observableArrayList ();
+    public ObservableList<String> indices = FXCollections.observableArrayList ();
+    public ArrayList<Carta> cartas;
+
+
+    public void initialize() throws JSONException, IOException, URISyntaxException {
         /*createTree();
         txtTitulo.setText("");
         txtDefinicion.setText("");
         txtCodigo.setText("");*/
-        txtColor.setText("HOLA!");
+        //txtColor.setText("HOLA!");
+        Refresh(new ActionEvent());
     }
 
     public void Refresh(ActionEvent actionEvent) throws URISyntaxException, IOException, JSONException {
         Api api= new Api();
-        ArrayList<Carta> cartas= api.getAllCartas();
+        cartas= api.getAllCartas();
 
 
         for (int i = 0; i <cartas.size() ; i++) {
             System.out.println(cartas.get(i).getName());
         }
+
+        for (int i = 0; i <cartas.size() ; i++) {
+            indices.add(""+i);
+            imagenes.add(cartas.get(i).imageUrl);
+            listaCartas.add(cartas.get(i).getName());
+
+        }
+        listViewCartas.setItems(indices);
+
+
+
+        listViewCartas.setCellFactory(param -> new ListCell<String>() {
+            private ImageView imageView = new ImageView();
+            @Override
+            public void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    Image IMAGE = new Image(imagenes.get(Integer.parseInt(item)),50,50,true,true,true);
+                    imageView.setImage(IMAGE);
+                    imageView.setCache(true);
+                    setText(listaCartas.get(Integer.parseInt(item)));
+                    setGraphic(imageView);
+                }
+            }
+        });
+
+    }
+
+    public void datos(MouseEvent mouseEvent) {
+        //int numeroCarta= Integer.parseInt(listViewCartas.getSelectionModel().getSelectedItems().toString());
+        String valor=listViewCartas.getSelectionModel().getSelectedItems().toString();
+        int numeroCarta= Integer.parseInt(valor.substring(1,valor.length()-1));
+
+        txtName.setText(cartas.get(numeroCarta).getName());
+        txtMana.setText(cartas.get(numeroCarta).getManaCost());
+        txtPower.setText(cartas.get(numeroCarta).getPower());
+        txtRarity.setText(cartas.get(numeroCarta).getRarity());
+        txtType.setText(cartas.get(numeroCarta).getType());
+        txtAreaText.setText(cartas.get(numeroCarta).getText());
+        Image img= new Image(cartas.get(numeroCarta).getImageUrl(),600,300,true,true,true);
+        Imagen.setCache(true);
+        Imagen.setFitHeight(600);
+        Imagen.setFitWidth(300);
+        Imagen.setImage(img);
+
 
     }
 }
